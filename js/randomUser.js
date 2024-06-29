95d4a1144596d25ca570d36cbf3347186395ef7c
Rubel Rudra <rubelrudra27@gmail.com>
2024-06-29 19:34:53 +0600

    create a random user API site

--- js/randomUser.js
+++ js/randomUser.js
@@ -1,25 +1,27 @@
-const loadUser=()=>{
-  fetch('https://randomuser.me/api/?gender=female')
-  .then(res=>res.json())
-  .then(data=>displayUser(data))
+const loadUser = () => {
+    fetch('https://randomuser.me/api/?gender=female')
+        .then(res => res.json())
+        .then(data => displayUser(data))
 }
 
-const displayUser=(data)=>{
-  console.log(data.results[0]);
-  const userContainer=document.getElementById('user-container');
-  const div=document.createElement('div');
-  div.classList.add('m-2');
-  div.innerHTML=`
-  <div class="card" style="width: 18rem;">
-        <img src="${data.results[0].picture.large}" class="card-img-top" alt="...">
+const displayUser = (user) => {
+    console.log(user);
+    const name = user.results[0].name.first + ' ' + user.results[0].name.last;
+    const users = document.getElementById('user-container');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card" style="width: 18rem;">
+        <img src="${user.results[0].picture.large}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h6>Name : ${data.results[0].name.first +' '+data.results[0].name.last}</h6>
-          <p class="card-text">Gender : ${data.results[0].gender}</p>
-          <p class="card-text">Email : ${data.results[0].email}</p>
-          <p class="card-text">Country : ${data.results[0].location.country}, City : ${data.results[0].location.city}</p>
+        <h5 class="card-title">Name : ${name}</h5>
+        <p class="card-text">Gender : ${user.results[0].gender}</p>
+        <p class="card-text">Country : ${user.results[0].location.country} , City : ${user.results[0].location.city}</p>
+        <p class="card-text">Email : ${user.results[0].email}</p>
+        <p class="card-text">Phone : ${user.results[0].phone}</p>
+        <a onclick="loadUser()" class="btn btn-primary">See Another</a>
         </div>
-      </div>
-  `
-  userContainer.appendChild(div);
+    </div>
+    `;
+    users.appendChild(div);
 }
 loadUser();
